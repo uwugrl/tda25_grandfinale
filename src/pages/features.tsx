41ac9cab0f30d1bb2aa535c9756b162ee0192c73
Lunya UwU :3 <mldkyt@proton.me>
2025-03-22 18:40:20 +0100

    fix Add extra features

--- src/pages/features.tsx
+++ src/pages/features.tsx
@@ -16,7 +16,8 @@ export default function Features() {
           <ul>
             <li><Typography>F-014 (30b) - Pro připojení využití QR kódu vygenerovaném na UI admina</Typography></li>
             <li><Typography>F-005 (20b) - Pokud dojde omylem k odpojení, má uživatel možnost so znovu připojit</Typography></li>
-            <li><Typography>F-009 (30b) - Pokud dojde omylem k odpojení, má uživatel možnost so znovu připojit</Typography></li>
+            <li><Typography>F-009 (30b) - Ukládání a zobrazení výsledků proběhlých místností do admin panelu. Zpětné zobrazení výsledků</Typography></li>
+            <li><Typography>F-004 (40b) - Prezentující před startem své prezentace krátce popíše svůj nápad. Správce má možnost nápady a uložit a zobrazit si je.</Typography></li>
           </ul>
       </div>
 
