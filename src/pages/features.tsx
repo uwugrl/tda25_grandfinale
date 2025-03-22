8b64824227943c2c362ba0a3d85d66e2ff3f275b
Lunya UwU :3 <mldkyt@proton.me>
2025-03-22 16:35:45 +0100

    features

--- src/pages/features.tsx
+++ src/pages/features.tsx
@@ -1,9 +1,12 @@
+import { Typography } from "@mui/joy";
 
 export default function Features() {
   return (
-    <div className="bg-white">
-        <h1 className="text-2xl font-bold text-center">Funkce naší aplikace</h1>
-        <ul className="list-disc ml-4">
+    <div className="w-2/3 m-auto">
+        <Typography level="h1">Funkce naší aplikace</Typography>
+        <ul>
+          <li><Typography>F-014 (30b) - Pro připojení využití QR kódu vygenerovaném na UI admina</Typography></li>
+          <li><Typography>F-005 (20b) - Pokud dojde omylem k odpojení, má uživatel možnost so znovu připojit</Typography></li>
         </ul>
     </div>
   )
